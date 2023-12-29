1ee0dd22e3a9d4f6cce7b29f8230e4189e00ac66
toshikistan <toshikistan@gmail.com>
2023-12-29 20:12:16 +0200

    initial commit

---
+++ hillel.py
@@ -0,0 +1,65 @@
+# userdata = int(input('Enter number:'))
+# multiplier = int(input('Enter multiplier:'))
+# result = userdata * multiplier
+# if result > 100:
+#     print('Uppi,', result)
+# else:
+#     print('Oh,', result)
+
+
+
+
+
+# name = input('Enter your name: ') 
+# greeting = f"Hello, {name}!"
+# print(greeting)
+
+
+
+
+
+# fruits = ["apple", "banana", "cherry"] 
+# score = int(input('Enter your number" '))
+# multiplier = int(input('Enter multiplier: '))
+# result = score * multiplier
+# if result >25:
+#     fruits.append("orange")
+# else:
+#     None
+# print(fruits)
+
+
+
+
+
+# print(20 < 15)
+# print(7 * 3)
+# print(25 / 4 * 5)
+# print(17 ** 4)
+# print(.2 * 7+ 3.5 * 4)
+# a = 4
+# b = int(input('Enter number 1: '))
+# c = int(input('Enter number 2: '))
+# result = (a ** 2, b * c)
+# print(result)
+# if a ** 2 == b * c:
+#     print('a^2=b*c')
+# elif a ** 2 < b * c:
+#     print('a^2<b*c')
+# else:
+#     print('a^2>b*c')
+
+# var_1 = 17
+# var_2 = 93
+# var_1 += var_2
+# print(var_1)
+# var_1 *= var_2
+# print(var_1)
+# var_1 -= var_2 * var_1
+# print(var_1)
+
+
+
+
+y = "Hello, World!" 
+print(id(y))
